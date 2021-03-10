b2405427ad6004b7388356a82ad4753133565fb0
Ali Ocasha <aliocasha@gmail.com>
2021-03-10 18:00:48 +0100

    Drawing a very simple board

--- Minimax TicTacToe/Minimax TicTacToe/Minimax_TicTacToe.py
+++ Minimax TicTacToe/Minimax TicTacToe/Minimax_TicTacToe.py
@@ -1,1 +1,23 @@
+import time
 
+class TicTacToe:
+    def __init__(self):
+        self.initialize_game()
+
+    def initialize_game(self):
+        self.current_state = [[".",".","."],
+                              [".",".","."],
+                              [".",".","."]]
+        self.player_turn = "X"
+
+    def draw_board(self):
+        print("---------")
+        for i in range(3):
+            for j in range(3):
+                print('{}|'.format(self.current_state[i][j]), end=" ")
+            print()
+            print("---------")
+
+
+Game = TicTacToe()
+Game.draw_board()
