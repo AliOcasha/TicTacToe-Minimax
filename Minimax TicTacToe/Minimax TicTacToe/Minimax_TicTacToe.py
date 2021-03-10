c5c0b83a721a228f23d64b47872026b0921e18ec
Ali Ocasha <aliocasha@gmail.com>
2021-03-10 18:24:50 +0100

    Defined ending Conditions and move validation

--- Minimax TicTacToe/Minimax TicTacToe/Minimax_TicTacToe.py
+++ Minimax TicTacToe/Minimax TicTacToe/Minimax_TicTacToe.py
@@ -1,5 +1,15 @@
 import time
 
+def clear(): 
+  
+    # for windows 
+    if name == 'nt': 
+        _ = system('cls') 
+  
+    # for mac and linux(here, os.name is 'posix') 
+    else: 
+        _ = system('clear')
+
 class TicTacToe:
     def __init__(self):
         self.initialize_game()
@@ -18,6 +28,48 @@ class TicTacToe:
             print()
             print("---------")
 
+    def is_valid(self, px,py):
+        if px < 0 or px > 2 or py < 0 or py > 2:
+            return False
+        
+        elif self.current_state[px][py] != ".":
+            return False
+        else:
+
+            return True
+
+    def is_end(self):
+        for i in range(0, 3):
+            if (self.current_state[0][i] != '.' and
+                self.current_state[0][i] == self.current_state[1][i] and
+                self.current_state[1][i] == self.current_state[2][i]):
+                return self.current_state[0][i]
+
+        for i in range(0, 3):
+            if (self.current_state[i] == ['X', 'X', 'X']):
+                return 'X'
+            elif (self.current_state[i] == ['O', 'O', 'O']):
+                return 'O'
+
+        if (self.current_state[0][0] != '.' and
+            self.current_state[0][0] == self.current_state[1][1] and
+            self.current_state[0][0] == self.current_state[2][2]):
+            return self.current_state[0][0]
+
+        if (self.current_state[0][2] != '.' and
+            self.current_state[0][2] == self.current_state[1][1] and
+            self.current_state[0][2] == self.current_state[2][0]):
+            return self.current_state[0][2]
+
+        for i in range(0, 3):
+            for j in range(0, 3):
+                if (self.current_state[i][j] == '.'):
+                    return None
+
+        return '.'
+
 
 Game = TicTacToe()
 Game.draw_board()
+
+
