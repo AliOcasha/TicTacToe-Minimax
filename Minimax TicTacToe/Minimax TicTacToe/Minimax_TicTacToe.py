818195c33017cf6ac0ae244ac3e1ce5a3bbd6a6a
Ali Ocasha <aliocasha@gmail.com>
2021-03-12 18:33:37 +0100

    Added GameLoop and Alternating Turns

--- Minimax TicTacToe/Minimax TicTacToe/Minimax_TicTacToe.py
+++ Minimax TicTacToe/Minimax TicTacToe/Minimax_TicTacToe.py
@@ -1,15 +1,5 @@
 import time
 
-def clear(): 
-  
-    # for windows 
-    if name == 'nt': 
-        _ = system('cls') 
-  
-    # for mac and linux(here, os.name is 'posix') 
-    else: 
-        _ = system('clear')
-
 class TicTacToe:
     def __init__(self):
         self.initialize_game()
@@ -40,36 +30,79 @@ class TicTacToe:
 
     def is_end(self):
         for i in range(0, 3):
-            if (self.current_state[0][i] != '.' and
+            if (self.current_state[0][i] != "." and
                 self.current_state[0][i] == self.current_state[1][i] and
                 self.current_state[1][i] == self.current_state[2][i]):
                 return self.current_state[0][i]
 
         for i in range(0, 3):
-            if (self.current_state[i] == ['X', 'X', 'X']):
-                return 'X'
-            elif (self.current_state[i] == ['O', 'O', 'O']):
-                return 'O'
+            if (self.current_state[i] == ["X", "X", "X"]):
+                return "X"
+            elif (self.current_state[i] == ["O", "O", "O"]):
+                return "O"
 
-        if (self.current_state[0][0] != '.' and
+        if (self.current_state[0][0] != "." and
             self.current_state[0][0] == self.current_state[1][1] and
             self.current_state[0][0] == self.current_state[2][2]):
             return self.current_state[0][0]
 
-        if (self.current_state[0][2] != '.' and
+        if (self.current_state[0][2] != "." and
             self.current_state[0][2] == self.current_state[1][1] and
             self.current_state[0][2] == self.current_state[2][0]):
             return self.current_state[0][2]
 
         for i in range(0, 3):
             for j in range(0, 3):
-                if (self.current_state[i][j] == '.'):
+                if (self.current_state[i][j] == "."):
                     return None
 
-        return '.'
+        return "."
+
+    def play(self):
+        while True:
+            self.draw_board()
+            self.result = self.is_end()
+
+            if self.result != None:
+                if self.result == "X":
+                    print("The Winner is X")
+                elif self.result == "O":
+                    print("The Winner is O")
+                elif self.result == ".":
+                    print("Its a Tie")
+
+                self.initialize_game()
+                return
+
+            if self.player_turn == "X":
+                while True:
+                    px = int(input("Insert a X-Coordinate: "))
+                    py = int(input("Insert a Y-Coordinate: "))
+
+                    if self.is_valid(px, py):
+                        self.current_state[px][py] = "X"
+                        self.player_turn = "O"
+                        break
+                    else:
+                        print('Invalid Move, Try Again')
+
+            elif self.player_turn == "O":
+                while True:
+                    px = int(input("Insert a X-Coordinate: "))
+                    py = int(input("Insert a Y-Coordinate: "))
+
+                    if self.is_valid(px, py):
+                        self.current_state[px][py] = "O"
+                        self.player_turn = "X"
+                        break
+                    else:
+                        print('Invalid Move, Try Again')
+        else:
+            print("Fatal Error!!!")
 
 
 Game = TicTacToe()
-Game.draw_board()
+Game.play()
+
 
 
