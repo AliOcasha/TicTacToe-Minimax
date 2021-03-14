ad06b1a41318fc6902e7c172ace88b85dc489b47
Ali Ocasha <aliocasha@gmail.com>
2021-03-14 16:26:14 +0100

    designed and coded min and max function

--- Minimax TicTacToe/Minimax TicTacToe/Minimax_TicTacToe.py
+++ Minimax TicTacToe/Minimax TicTacToe/Minimax_TicTacToe.py
@@ -72,6 +72,65 @@ class TicTacToe:
 
         return "."
 
+    def max(self):
+        maxv = -2
+        px = None
+        py = None
+        
+        result = self.is_end()
+
+        if result == "X":
+            return (-1, 0, 0)
+        elif result == "O":
+            return (1, 0, 0)
+        elif result ==".":
+            return (0, 0, 0)
+        else:
+            print("Fatal Error!")
+
+        for i in range(0, 3):
+            for j in range(0, 3):
+                if self.current_state[i][j] == ".":
+                    self.current_state[i][j] == "0"
+                    (m, min_i, min_j) = self.min()
+
+                    if m > maxv:
+                        maxv = m
+                        px = i
+                        py = j
+                    self.current_state[i][j] = "."
+        return (maxv, px, py)
+
+
+    def min(self):
+        minv = 2
+        px = None
+        py = None
+
+        result = self.is_end()
+
+        if result == "X":
+            return (-1, 0, 0)
+        elif result == "O":
+            return (1, 0, 0)
+        elif result == ".":
+            return (0, 0, 0)
+        else:
+            print("Fatal Error!")
+
+        for i in range(0, 3):
+            for j in range(0, 3):
+                if self.current_state[i][j] == ".":
+                    self.current_state[i][j] = "X"
+                    (m, max_i, max_i) = self.max()
+
+                    if m < minv:
+                        minv = m
+                        px = i
+                        py = j
+                    self.current_state[i][j] = "."
+        return (minv, px, py)
+
     def play(self):
         while True:
             self.draw_board()
